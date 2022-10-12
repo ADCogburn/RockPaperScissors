0094a5a892b1758e5feddca575a1c1b190c3f1de
andre <Andrew.d.cogburn@gmail.com>
2022-10-12 18:02:33 -0500

    Game version 1.0

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -7,16 +7,17 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
+            string playerChoice = "a";
+            string computerChoice;
+            
             Console.WriteLine("Welcome to Rock, Paper, and Scissors!");
             Console.WriteLine("Please choose an option: Rock, Paper, or Scissors.");
 
-            var answered = false;
-
             // Check if the player made a valid choice.
-            // Currently 'crashes' if the choice is invalid; I need to find a way to push it back to the top.
+            var answered = false;
             while (answered == false)
             {
-                string playerChoice = Console.ReadLine();
+                playerChoice = Console.ReadLine();
                 switch (playerChoice)
                 {
                     case "rock":
@@ -33,6 +34,67 @@ namespace RockPaperScissors
                         break;
                 }
             }
+
+            Random computerNumber = new Random();
+            int computerRoll = computerNumber.Next(0,2);
+            
+            if (computerRoll == 0)
+            {
+                computerChoice = "rock";
+            } else if (computerRoll == 1) {
+                computerChoice = "paper";
+            } else
+            {
+                computerChoice = "scissors";
+            }
+
+            Console.WriteLine($"The computer's choice is {computerChoice}.");
+
+            //Player is rock.
+            if (playerChoice == "rock" && computerChoice == "rock")
+            {
+                Console.WriteLine("Tie game!");
+            }
+            else if (playerChoice == "rock" && computerChoice == "scissors")
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (playerChoice == "rock" && computerChoice == "paper")
+            {
+                Console.WriteLine("You lose!");
+            }
+
+
+            //Player is paper.
+            if (playerChoice == "paper" && computerChoice == "rock")
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (playerChoice == "paper" && computerChoice == "paper")
+            {
+                Console.WriteLine("Tie game!");
+            }
+            else if (playerChoice == "paper" && computerChoice == "scissor")
+            {
+                Console.WriteLine("You lose!");
+            }
+
+            //Player is scissors.
+            if (playerChoice == "scissors" && computerChoice == "rock")
+            {
+                Console.WriteLine("You lose!");
+            }
+            else if (playerChoice == "scissors" && computerChoice == "paper")
+            {
+                Console.WriteLine("You win!");
+            }
+            else if (playerChoice == "scissors" && computerChoice == "scissors")
+            {
+                Console.WriteLine("Tie game!");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+
         }
     }
 }
